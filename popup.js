1367fac2e8f592422c21fd2f26cf4afca5bab213
Jonny-Cho <andante2183@gmail.com>
2019-09-10 10:07:09 +0900

    Add like button (Todo: have to change the toggle logic)

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
-let btn = document.getElementById('giveHeart');
+let heartButton = document.getElementById('giveHeart');
+let likeButton = document.getElementById('giveLike');
 
-btn.onclick = function(){
+heartButton.onclick = function(){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
       tabs[0].id,
@@ -8,6 +9,22 @@ btn.onclick = function(){
         `
         var unhearted = document.querySelectorAll("yt-icon#unhearted");
         for (let s of unhearted){
+          s.click();
+        }
+        `
+      }
+    );
+  });
+};
+
+likeButton.onclick = function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code:
+        `
+        var unliked = document.querySelectorAll("ytd-toggle-button-renderer#like-button");
+        for (let s of unliked){
           s.click();
         }
         `
